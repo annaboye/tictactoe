335b08c5b127f9067cf74b10981d9b4d1fe0bb80
annaboye <boye.anna@gmail.com>
2023-04-11 10:00:00 +0200

    (feat): add a gameboard

---
+++ src/components/Board.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+
+
+</script>
+
+<template>
+  <div class="board">
+    <div class ="square" v-for="n in 9" :key="n"></div>
+  </div>
+</template>
+
+<style scoped>
+.board {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: space-around;
+    align-items: center;
+   background-color: rgb(169, 205, 169);
+    height:350px;
+    width: 350px;
+    margin: 0 auto;
+}
+.square {
+    width: 30%;
+    height: 30%;
+    
+    border: 1px solid rgb(64, 60, 60);;
+}
+</style>
